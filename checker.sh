5634dfc9f69b0df23c54ca3adf5111891e795e03
Aleksandr Saraikin <alex@sourcestream.de>
2015-03-16 22:08:20 +0100

    Use webiopi

--- checker.sh
+++ checker.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
+source env.conf
+
+function pin {
+  curl -s -o /dev/null -XPOST http://$USER:$PASSWORD@localhost:8000/GPIO/$1/function/$2
+}
+
+function modem_on {
+  pin $PIN IN
+}
+
+function modem_off {
+  pin $PIN OUT
+}
+
 function check {
-  ping -w 5 -c 5 -q 8.8.8.8
+  ping -w 1 -c 5 -q 8.8.8.8
   status=$?
   
-  if [[ $status == 0 ]]; then
+  if [[ $status == 1 ]]; then
     echo "Connection Alive"
   else
     echo "Connection Dead. Switching the modem off"
-    gpio -g write 4 0
+    modem_off
     sleep 10
-    gpio -g write 4 1
+    modem_on
     echo "Switching the modem on"
   fi
 }
